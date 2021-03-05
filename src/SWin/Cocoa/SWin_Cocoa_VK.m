a649df6cc2446ee1acbb84694ff2bc21d781e0a2
sharhar <wiishahar@gmail.com>
2021-03-04 16:03:46 -0800

    MacOS vulkan patch

--- src/SWin/Cocoa/SWin_Cocoa_VK.m
+++ src/SWin/Cocoa/SWin_Cocoa_VK.m
@@ -4,16 +4,16 @@
 #include <dlfcn.h>
 #import <QuartzCore/CAMetalLayer.h>
 
-typedef struct __sWin_Cocoa_VkMacOSSurfaceCreateInfoMVK {
-	int32_t            		        sType;
-	const void*                     pNext;
-	uint32_t                        flags;
-	const void*                     pView;
-} __sWin_Cocoa_VkMacOSSurfaceCreateInfoMVK;
+typedef struct __sWin_Cocoa_VkMetalSurfaceCreateInfoEXT {
+    int32_t sType;
+    const void* pNext;
+    uint32_t flags;
+    const CAMetalLayer* pLayer;
+} __sWin_Cocoa_VkMetalSurfaceCreateInfoEXT;
 
 typedef void (*__sWin_Cocoa_PFN_vkVoidFunction)(void);
 typedef __sWin_Cocoa_PFN_vkVoidFunction(*__sWin_Cocoa_PFN_vkGetInstanceProcAddr)(void* instance, const char* pName);
-typedef int32_t (*__sWin_Cocoa_PFN_vkCreateMacOSSurfaceMVK)(void* instance, const __sWin_Cocoa_VkMacOSSurfaceCreateInfoMVK* pCreateInfo, const void* pAllocator, void* pSurface);
+typedef int32_t (*__sWin_Cocoa_PFN_vkCreateMetalSurfaceEXT)(void* instance, const __sWin_Cocoa_VkMetalSurfaceCreateInfoEXT* pCreateInfo, const void* pAllocator, void* pSurface);
 
 __sWin_Cocoa_PFN_vkGetInstanceProcAddr __sWin_Cocoa_vkGetInstanceProcAddr;
 
@@ -29,7 +29,7 @@ SResult swInitVK() {
 	
 	__sWin_Cocoa_extensions = malloc(sizeof(char*) * 2);
 	__sWin_Cocoa_extensions[0] = "VK_KHR_surface";
-	__sWin_Cocoa_extensions[1] = "VK_MVK_macos_surface";
+	__sWin_Cocoa_extensions[1] = "VK_EXT_metal_surface";
 	
 	return SWIN_OK;
 }
@@ -47,14 +47,16 @@ char** swGetRequiredExtensionsVK(uint32_t* count) {
 int32_t swCreateWindowSurfaceVK(void* instance, SView* view, void* allocator, void* surface) {
 	SWin_Cocoa_View* _view = (SWin_Cocoa_View*)view;
 	
-	[_view->view setWantsLayer:YES];
-	[_view->view setLayer:[CAMetalLayer layer]];
+	CAMetalLayer* metalLayer = [CAMetalLayer layer];
 	
-	__sWin_Cocoa_VkMacOSSurfaceCreateInfoMVK surfaceCreateInfo;
-	surfaceCreateInfo.sType = 1000123000; // this is the integer value of VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK
+	[_view->view setWantsLayer:YES];
+	[_view->view setLayer:metalLayer];
+	
+	__sWin_Cocoa_VkMetalSurfaceCreateInfoEXT surfaceCreateInfo;
+	surfaceCreateInfo.sType = 1000217000; // this is the integer value of VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT
 	surfaceCreateInfo.pNext = NULL;
 	surfaceCreateInfo.flags = 0;
-	surfaceCreateInfo.pView = _view->view;
+	surfaceCreateInfo.pLayer = metalLayer;
 	
-	return ((__sWin_Cocoa_PFN_vkCreateMacOSSurfaceMVK)swGetProcAddressVK(instance, "vkCreateMacOSSurfaceMVK"))(instance, &surfaceCreateInfo, allocator, surface);
+	return ((__sWin_Cocoa_PFN_vkCreateMetalSurfaceEXT)swGetProcAddressVK(instance, "vkCreateMetalSurfaceEXT"))(instance, &surfaceCreateInfo, allocator, surface);
 }
